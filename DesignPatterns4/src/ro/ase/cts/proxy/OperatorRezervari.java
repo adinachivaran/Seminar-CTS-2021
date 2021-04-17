3a8c1bcc7366ca42179a79a9ff2e91709c67f150
Adina Chivaran <chivaranadina18@stud.ase.ro>
2021-04-17 13:56:19 +0300

    proxy

---
+++ DesignPatterns4/src/ro/ase/cts/proxy/OperatorRezervari.java
@@ -0,0 +1,12 @@
+package ro.ase.cts.proxy;
+
+public class OperatorRezervari implements IOperatorRezervari{
+
+	@Override
+	public void realizeazaRezervare(String numeClient, int nrPersoane) {
+		System.out.println("A fost realizata rezervarea pentru "+nrPersoane+" pe numele "+numeClient);
+		
+	}
+	
+
+}
---
+++ DesignPatterns4/src/ro/ase/cts/proxy/ProxyOperatorRezervari.java
@@ -0,0 +1,27 @@
+package ro.ase.cts.proxy;
+
+public class ProxyOperatorRezervari implements IOperatorRezervari{
+	
+	private OperatorRezervari operator;
+	private int nrMinimPersoane;
+
+
+	public ProxyOperatorRezervari(OperatorRezervari operator, int nrMinPers) {
+		super();
+		this.operator = operator;
+		this.nrMinimPersoane=nrMinPers;
+	}
+
+
+	@Override
+	public void realizeazaRezervare(String numeClient, int nrPersoane) {
+		if(nrPersoane>=nrMinimPersoane) {
+			operator.realizeazaRezervare(numeClient, nrPersoane);
+		}
+		else {
+			System.out.println("Va rugam veniti fara rezervare!");
+		}
+		
+	}
+
+}
